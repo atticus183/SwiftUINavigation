0ab7f13647ef9f18b6c7213c67c62f82a26103dd
Josh R <atticus183@gmail.com>
2023-09-09 15:56:07 -0500

    Initial commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Josh Rondestvedt on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
